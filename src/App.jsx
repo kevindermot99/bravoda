2c8e2e5e07a43936ab328fa16d17ba5e6951bb4e
kevindermot99 <mbonimpayekevin@gmail.com>
2024-09-17 18:18:13 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import { HashRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import NewLesson from './pages/NewLesson'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
       <HashRouter>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/new' element={<NewLesson />} />
         </Routes>
       </HashRouter>
     </>
