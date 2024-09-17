8b87da57b859293eadaa4f02feaeaaf35e73f410
kevindermot99 <mbonimpayekevin@gmail.com>
2024-09-17 18:18:17 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "../components/Sidebar";
 import { LuMic, LuPanelRight, LuPlus } from "react-icons/lu";
-
+import { Link } from "react-router-dom";
 function Home() {
   return (
     <div className="flex items-start justify-start h-svh text-text-black dark:text-text-white ">
@@ -10,18 +10,16 @@ function Home() {
         <div className="w-full min-h-full overflow-auto bg-white dark:bg-dark-card rounded-xl flex flex-col">
           {/* topbar */}
           <div className="w-full flex items-center justify-between p-3 border-b-[1px] border-stone-100 dark:border-gray-700/20">
-            <h1 className="text-sm px-2 font-medium ">
-              Lesson title: Introduction to Linux
-            </h1>
+            <h1 className="text-sm px-2 font-medium ">Dashboard</h1>
             <div className="flex items-center justify-center gap-1">
               <button className="flex items-center justify-center py-[7px] px-3 rounded-lg tracking-tight font-medium text-text-black/80 dark:text-text-white gap-1 text-sm bg-stone-100 dark:bg-dark-hover-color hover:opacity-80">
                 <LuMic className="text-lg" />
                 <p>Lecture mode</p>
               </button>
-              <button className="flex items-center justify-center py-[7px] px-3 rounded-lg tracking-tight font-medium text-text-black/80 dark:text-text-white gap-1 text-sm bg-transparent hover:bg-stone-200 dark:hover:bg-dark-hover-color">
+              <Link to={'/new'} className="flex items-center justify-center py-[7px] px-3 rounded-lg tracking-tight font-medium text-text-black/80 dark:text-text-white gap-1 text-sm bg-transparent hover:bg-stone-200 dark:hover:bg-dark-hover-color">
                 <LuPlus className="text-lg" />
                 <p>Create Lesson</p>
-              </button>
+              </Link>
               <button className="flex items-center justify-center py-[7px] px-2 rounded-lg tracking-tight font-medium text-text-black/80 dark:text-text-white gap-1 text-sm bg-transparent hover:bg-stone-200 dark:hover:bg-dark-hover-color">
                 <LuPanelRight className="text-lg" />
               </button>
@@ -34,13 +32,13 @@ function Home() {
             <p className="text-sm tracking-tight max-w-[340px] text-left opacity-70 font-medium">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
               alias, nam minima dolorem, pariatur dolore maiores incidunt
-              officiis totam voluptas saepe, 
+              officiis totam voluptas saepe,
             </p>
             <div className="flex items-center justify-start gap-2 w-full max-w-[340px]">
-              <button className="flex items-center justify-center py-[7px] px-3 rounded-lg tracking-tight font-medium text-white bg-main-color gap-1 text-sm ">
+              <Link to={'/new'} className="flex items-center justify-center py-[7px] px-3 rounded-lg tracking-tight font-medium text-white bg-main-color gap-1 text-sm ">
                 <LuPlus className="text-lg" />
                 <p>Create Lesson</p>
-              </button>
+              </Link>
               <button className="flex items-center justify-center py-[7px] px-3 rounded-lg tracking-tight font-medium text-text-black/80 dark:text-text-white gap-1 text-sm bg-stone-100 dark:bg-dark-hover-color hover:opacity-80">
                 <p>Documentation</p>
               </button>
